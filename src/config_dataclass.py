e03f032c13d33e6fd54c4817e225253976ea3a89
Christos Lathourakis <christos.lathourakis@tno.nl>
2024-07-23 20:54:15 +0200

    Implemented a dataclass and toml file to load hyperparameters.

---
+++ src/config_dataclass.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from pathlib import Path
+import toml
+from typing import Any
+
+
+@dataclass
+class Config:
+    datafile: str
+    drop_cols: list[str]
+    rename_dict: dict[str, str]
+    filter_bounds: dict[str, Any]
+    predictors: list[str]
+    target: list[str]
+    training_ratio: float
+    scaling_mode: str
+    gpr_params: dict[str, Any]
+    model_path: str
+
+
+def load_config(filepath: Path) -> Config:
+    with open(filepath, 'r') as f:
+        config_dict = toml.load(f)
+    return Config(**config_dict)
