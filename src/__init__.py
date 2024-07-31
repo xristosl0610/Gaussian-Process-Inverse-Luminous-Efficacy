278389c1b2d0fad20e164a1e3e14493da8044048
Christos Lathourakis <christos.lathourakis@tno.nl>
2024-07-31 19:32:42 +0200

    Added benchmark ML models in init

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,9 @@
 from pathlib import Path
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import make_pipeline
+
 
 ROOTDIR = Path(__file__).parent.parent
 CONFIGDIR = ROOTDIR.joinpath('config')
@@ -14,3 +19,10 @@ TDIR = 'in'
 MAJOR = 5.0
 MINOR = 3.0
 STYLE = 'default'
+
+BENCHMARK_MODELS = {
+    'linear': LinearRegression(),
+    'polynomial2': make_pipeline(PolynomialFeatures(degree=2), LinearRegression()),
+    'polynomial3': make_pipeline(PolynomialFeatures(degree=3), LinearRegression()),
+    'random_forest': RandomForestRegressor(),
+}
